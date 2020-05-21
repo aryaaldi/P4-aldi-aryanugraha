95da663477cb55993f17c3bc5fb25115166b2ab3
aryaaldi <65721175+aryaaldi@users.noreply.github.com>
2020-05-21 22:28:29 +0800

    Create jobshet 2 C++.cpp
    Buatlah program mengisi dan menampilkan elemen array dengan index array sesuai dengan NIM terakhir setiap mahasiswa

---
+++ jobshet 2 C++.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"Aldi Aryanugraha Mahendra\n\n";
+	string nim[5];
+	
+	cout<<"Masukkan nilai 1 : "; cin>>nim[0];
+	cout<<"\nMasukkan nilai 2 : "; cin>>nim[1];
+	cout<<"\nMasukkan nilai 3 : "; cin>>nim[2];
+	cout<<"\nMasukkan nilai 4 : "; cin>>nim[3];
+	cout<<"\nMasukkan nilai 5 : "; cin>>nim[4];
+	
+	cout<<endl;
+	
+	cout<< "Data NIM Mahasisawa yang anda input : "<<endl;
+	cout << "Nim ke-1 adalah " << nim[0] << endl;
