16b89c21f8c3b4029265d6c402a6e29b7fe94546
aryaaldi <65721175+aryaaldi@users.noreply.github.com>
2020-05-21 22:59:48 +0800

    Create jobshet 10 C++.cpp
    Buatlah program matriks tiga dimensi dengan menggunakan NIM mahasiswa

---
+++ jobshet 10 C++.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+main(){
+char nama [3][5][7];
+int i,j;
+for(i=0;i<3;i++){
+for (j=0;i<3;i++){
+cout<<"Masukan Nim: ["<<i<<"]["<<j<<"]=";
+cin>>nama[i][j];}
+}
+}
