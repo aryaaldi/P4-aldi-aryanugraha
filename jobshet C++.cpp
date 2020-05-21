742ebde70a820c1456afb2350945bc2958d7d8f3
aryaaldi <65721175+aryaaldi@users.noreply.github.com>
2020-05-21 22:24:49 +0800

    Create jobshet C++.cpp
    Buatlah program array satu dimensi yang menampilkan NIM masing-masing mahasiswa

---
+++ jobshet C++.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"Aldi Aryanugraha Mahendra\n\n";
+int nim[5]= {1,9,0,1,8};
+	
+	cout << "Nim ke-1 adalah " << nim[0] << endl;
+	cout << "Nim ke-2 adalah " << nim[1] << endl;
+	cout << "Nim ke-3 adalah " << nim[2] << endl;
+	cout << "Nim ke-4 adalah " << nim[3] << endl;
+	cout << "Nim ke-5 adalah " << nim[4] << endl;
+
+}
+
+
